abb54a1a8d6913ae7a864872608129fe085dc26d
Ben Cox <Ben@Benjojo.co.uk>
2013-12-30 23:32:09 +0000

    Convert it to a LAB array

--- Prototype/Prototype/Program.cs
+++ Prototype/Prototype/Program.cs
@@ -8,6 +8,18 @@ namespace Prototype
         static void Main(string[] args)
         {
             Image LoadedImg = Image.FromFile("sample.jpg");
+            Bitmap LoadedBit = (Bitmap)LoadedImg;
+            CIELab[,] px = new CIELab[LoadedImg.Width, LoadedImg.Height];
+
+            for (int x = 0; x < LoadedImg.Width - 1; x++)
+            {
+                for (int y = 0; y < LoadedImg.Height - 1; y++)
+                {
+                    Color Pnt = LoadedBit.GetPixel(x,y);
+                    px[x, y] = ColorConv.RGBtoLab(Pnt.R, Pnt.G, Pnt.B);
+                }
+            }
+
         }
     }
 }
