cb01b4be3d90ba95853ffdcaff27800776bc0862
Lex Robinson <lexi@lexi.org.uk>
2014-03-07 21:34:22 +0000

    Get collapse.js into a more packagey form

--- src/shared/collapse.js
+++ src/shared/collapse.js
@@ -1,9 +1,8 @@
-var colorCollapse;
-(function() {
+(function(exports, labcol) {
     'use strict';
     var magic = new labcol();
 
-    function colMagic(r, g, b) {
+    exports.colorCollapse = function(r, g, b) {
         var labpx = magic.RGBtoLab(r, g, b);
         var res = (labpx.a + labpx.b) / 2;
         labpx.a = res;
@@ -11,6 +10,4 @@ var colorCollapse;
 
         return magic.LabtoRGB(labpx.l, labpx.a, labpx.b);
     }
-    // Exports
-    colorCollapse = colMagic;
-})();
+})(window, labcol);
